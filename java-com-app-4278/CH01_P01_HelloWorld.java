86ab768cee7e9624fcbd10941a7c2d19852401ff
gkHell <gpkmyworld@gmail.com>
2024-04-26 17:17:08 +0530

    26/04/2024⠀⎦⎡⠀17:17⠀⎦⎢⠀Friday

---
+++ java-com-app-4278/CH01_P01_HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class CH01_P01_HelloWorld{
+
+    // data + method
+
+    public static void main(String[] args) {
+        
+        String name = "Jorge";
+        System.out.println("Hello There !! "+name);
+    }
+
+}
+
+// JorgeCarter => class  name => System
+// jorgeCarter => object name => out
