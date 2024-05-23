bb7fcbfb93804a258e0e45738b9d373d11b47979
gkHell <gpkmyworld@gmail.com>
2024-05-23 16:16:12 +0530

    23/05/2024⠀⎦⎡⠀16:16⠀⎦⎢⠀Thursday

---
+++ static-web-4249/www.example.com/src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+
+  $.ajax({
+    url: "./statics/footer.html",
+    success: function (response) {
+        
+      $(".footer").html(response)
+    },
+  });
+});
