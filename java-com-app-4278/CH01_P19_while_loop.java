d6f8e89e83036bddb3a32261f818b83dee89cccc
gkHell <gpkmyworld@gmail.com>
2024-06-01 19:08:00 +0530

    01/06/2024⠀⎦⎡⠀19:07⠀⎦⎢⠀Saturday

---
+++ java-com-app-4278/CH01_P19_while_loop.java
@@ -0,0 +1,13 @@
+public class CH01_P19_while_loop {
+    public static void main(String[] args) {
+
+        // initial, condition, increment/decrement
+
+        int line = 10;
+
+        while (line >= 1) {
+            System.out.println("\tLine " + line);
+            line--;
+        }
+    }
+}
