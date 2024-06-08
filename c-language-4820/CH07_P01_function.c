a462e575ee8dd9035c552616bef7465b5159d6d7
gkHell <gpkmyworld@gmail.com>
2024-06-08 16:14:44 +0530

    08/06/2024 16:14 Saturday

---
+++ c-language-4820/CH07_P01_function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void msg()
+{
+    printf("Hello Harshita.\n");
+}
+
+void gretting()
+{
+    printf("Good Morning...\n");
+}
+void main()
+{
+
+    msg();
+    printf("How are you..\n");
+    gretting();
+}
