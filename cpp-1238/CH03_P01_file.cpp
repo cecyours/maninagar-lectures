0e404e2bb7bc140ba7c70bf44d2bee2de4f3ef5f
gkHell <gpkmyworld@gmail.com>
2024-07-18 16:36:07 +0530

    18/07/2024 16:36 Thursday

---
+++ cpp-1238/CH03_P01_file.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "CH03_P01_header.h"
+using namespace std;
+int main()
+{
+    int x,y;
+
+    cout<<"Enter 2 numbers  : ";
+    cin>>x>>y;
+
+    cout<<"the max is "<<max(x,y)<<endl;
+
+
+}
