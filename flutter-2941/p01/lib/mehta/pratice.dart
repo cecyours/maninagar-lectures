f5b7ad14215360efe916d272e0adba83289ab392
gkHell <gpkmyworld@gmail.com>
2024-07-29 19:24:01 +0530

    29/07/2024 19:24 Monday

--- flutter-2941/p01/lib/mehta/pratice.dart
+++ flutter-2941/p01/lib/mehta/pratice.dart
@@ -1,78 +1,135 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Pratice extends StatefulWidget {
   @override
   _Pratice createState() {
-
     return _Pratice();
-
-   }
+  }
 }
 
 class _Pratice extends State<Pratice> {
-  int number_ = 0;
-
-
-  void getNumber() {
-    setState(() {
-      number_ = Random().nextInt(1000);
-    });
-  }
-
-
-  void prevNumber() {
-    setState(() {
-      number_--;
-    });
-  }
-
-  void addNumber(){
-    setState(() {
-      number_++;
-    });
-  }
-
-
+  void fun() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("App by maulesh"),
-
+        title: Text("yo"),
       ),
       body: Material(
-        color: Colors.deepPurpleAccent,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-          child: Center(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 30,horizontal: 30),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                ElevatedButton(onPressed: getNumber, child: Text("Random number"),),
-                ElevatedButton(onPressed: prevNumber, child: Text("-"),),
-                ElevatedButton(onPressed: addNumber, child: Text("+"),),
-                Text("number is $number_"),
-                SizedBox(height: 20,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    label:Text("User Name",style: TextStyle(color: Colors.white),),
-                    border: OutlineInputBorder(borderSide: BorderSide()),
-
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("C"),),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("/")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("X")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("c")),
+                  ],
                 ),
-                SizedBox(height: 20,),
-
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                label:Text("Password",style: TextStyle(color: Colors.white),),
-                border: OutlineInputBorder(borderSide: BorderSide()),
-
-              ),
-            ),
+                SizedBox(height: 10,)
+              ,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("7"),),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("8")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("9")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("-")),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("4"),),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("5")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("6")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("+")),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("1"),),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("2")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("3")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("=")),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("%"),),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("0")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text(".")),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(onPressed: fun, child: Text("C"),),
+                  ],
+                ),
               ],
             ),
           ),
@@ -80,5 +137,4 @@ class _Pratice extends State<Pratice> {
       ),
     );
   }
-
 }
