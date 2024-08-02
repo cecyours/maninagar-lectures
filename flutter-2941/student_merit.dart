011541ba52888ac0689a1a69bc4108fd9ea5e353
gkHell <gpkmyworld@gmail.com>
2024-08-02 19:22:32 +0530

    02/08/2024 19:22 Friday

--- flutter-2941/student_merit.dart
+++ flutter-2941/student_merit.dart
@@ -12,23 +12,28 @@ class StudentMerit {
   }
 }
 
+ Future<void> setData(List list) async {
+  await dummer(list);
+}
+
+Future<void> dummer(List list) async {
+  int n = 1000; // Adjust this number as needed
+  for (int i = 0; i < n; i++) {
+    double m = (Random().nextDouble() * 10);
+    String marks = m.toStringAsFixed(2);
+    list.add(StudentMerit("Student ${i + 1}", double.parse(marks), i + 1));
+  }
+  // Simulate a delay to represent async operation
+  await Future.delayed(Duration(milliseconds: 100));
+}
 void main() {
   List list = <StudentMerit>[];
 
   var i;
-  var n = 1000000000;
-  for (i = 0; i < n; i++) {
-    double m = (Random().nextDouble() * 100);
-    String marks = m.toStringAsFixed(2);
-    list.add(StudentMerit("Student ${i + 1}", double.parse(marks), i + 1));
-  }
-  print("Hello World...");
-  print("\n--------------------\n");
 
-  for (i = 0; i < list.length; i++) {
-    StudentMerit s = list[i];
-    print(" ${s.id} | ${s.name} | ${s.marks}");
-  }
+  print("\n--------------------\n");
+  setData(list);
+  print("Hello World...");
 
   list.sort((a, b) => b.marks.compareTo(a.marks));
 
