f5052c2d1510cf60fe48f81ddb4c9e3e39ce16f4
gkHell <gpkmyworld@gmail.com>
2024-08-06 19:12:51 +0530

    06/08/2024 19:12 Tuesday

--- flutter-2941/p01/lib/master/CH04_P03_GridView.dart
+++ flutter-2941/p01/lib/master/CH04_P03_GridView.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+
 class Ch04P03Gridview extends StatefulWidget {
+  const Ch04P03Gridview({super.key});
+
   @override
   Ch04P03GridViewState createState() {
     return Ch04P03GridViewState();
@@ -8,35 +11,49 @@ class Ch04P03Gridview extends StatefulWidget {
 }
 
 class Ch04P03GridViewState extends State<Ch04P03Gridview> {
-  
-  late VideoPlayerController _controller = VideoPlayerController.networkUrl(Uri.parse("https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4"));
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'))
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("kites"),
-      ),
-      body: Material(
-        child: GridView.count(
-            crossAxisCount: 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            childAspectRatio: 1,
-            children: [
-              Image.network(
-                  "https://picsum.photos/200"),
-              Image.network(
-                  "https://picsum.photos/200"),
-
-
-              Container(
-                color: Colors.yellow,
-              ),
-              Container(
-                color: Colors.green,
-              )
-            ]),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("kites"),
+        ),
+        body: Material(
+            child: Center(
+                child: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: _controller.value.isInitialized
+                  ? VideoPlayer(_controller)
+                  : Container(
+                      child: Text("waiting...."),
+                    ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  });
+                },
+                child: Icon(_controller.value.isPlaying
+                    ? Icons.pause_circle
+                    : Icons.play_arrow))
+          ],
+        ))));
   }
 }
