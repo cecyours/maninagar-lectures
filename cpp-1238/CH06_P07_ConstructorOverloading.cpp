6ba2d7582bae06867baa9640199b08bf30be5854
gkHell <gpkmyworld@gmail.com>
2024-08-29 18:01:49 +0530

    29/08/2024 18:01 Thursday

---
+++ cpp-1238/CH06_P07_ConstructorOverloading.cpp
@@ -0,0 +1,23 @@
+    #include <iostream>
+    using namespace std;
+
+    class Book{
+
+        private:
+        int id;
+        string name;
+        double price;
+
+        public:
+
+            Book()
+            {
+                id = 10;
+                name = "master";
+                price = 30;
+            }
+    };
+    int main()
+    {
+        return 0;
+    }
