3e0f2da3636583a0d4fce236dae62141544ababb
gkHell <gpkmyworld@gmail.com>
2024-09-06 19:42:14 +0530

    06/09/2024 19:42 Friday

---
+++ python-2569/CH02_P02_variables.py
@@ -0,0 +1,37 @@
+
+# int 
+i = 39
+
+print(type(i),i)
+
+# float
+f = float(3932482848292939.38383240)
+print(type(f),f)
+
+#complex
+i = 3+4j
+print(type(i),i)
+
+# string
+s = 'a';
+print(type(s),s)
+
+
+# boolean
+b = 3==4
+print(type(b),b)
+
+
+# list
+names = ["Rupesh Bhai","Kuch Nai","Denver"];
+names[0]="master"
+names.append("Denver is king")
+print(type(names),names)
+
+# tuple
+names = ("Rupesh Bhai","Kuch Nai","Denver");
+# names[0]="master"
+print(type(names),names)
+
+#t
+
