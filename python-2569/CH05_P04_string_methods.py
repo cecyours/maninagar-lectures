b85eb560ccba7a2aed4af87bc715cffa36407668
gkhell <gpkmyworld@gmail.com>
2024-11-15 19:55:19 +0530

    15/11/2024 19:55 Friday

--- python-2569/CH05_P04_string_methods.py
+++ python-2569/CH05_P04_string_methods.py
@@ -54,3 +54,7 @@ print("{:>15} | {:20} | {:20}".format("find",s,ans))
 s = "ktei10"
 ans = s.isalnum()
 print("{:>15} | {:20} | {:20}".format("isalnum",s,ans))
+
+s = "apple,banana,cherry"
+ans = s.split(",")
+print("{:>15} | {:20} ".format("split",s),ans)
