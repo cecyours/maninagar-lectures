9b9cea1c5adab48d8177405ad856162465b4a078
gkhell <gpkmyworld@gmail.com>
2024-11-22 14:40:42 +0530

    22/11/2024 14:40 Friday

---
+++ cpp-8128/CH11_P01_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[100] = {'a','v','k','\0','k','o'};
+    char kites[] = "Hello Kites"; 
+
+    cout<<"string  : "<<name<<endl;
+    cout<<" kites  : "<<kites<<endl;
+
+
+}
