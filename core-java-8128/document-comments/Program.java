251ff7e3dc23100b40b5a732e45a677692d703c2
gkhell <gpkmyworld@gmail.com>
2024-12-18 18:45:56 +0530

    18/12/2024 18:45 Wednesday

---
+++ core-java-8128/document-comments/Program.java
@@ -0,0 +1,36 @@
+// package document-comments;
+
+import java.io.IOException;
+
+/***
+ * 
+ * <p style="background:red;">
+ * <h1>This is my code</h1>
+ * </p>
+ * 
+  */
+public class Program extends IOException {
+    
+
+
+    /**
+     * My Comment
+     * @return
+      */
+    public double setData()
+    {
+        return 0.3;
+    }
+
+    /**
+     * Happier myself
+     * 
+      */
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
+
+
