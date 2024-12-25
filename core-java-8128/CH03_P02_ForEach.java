398f5943981bd71222f5da11dfe0914769351bf0
gkhell <gpkmyworld@gmail.com>
2024-12-25 14:28:56 +0530

    25/12/2024 14:28 Wednesday

---
+++ core-java-8128/CH03_P02_ForEach.java
@@ -0,0 +1,14 @@
+public class CH03_P02_ForEach {
+    public static void main(String[] args) {
+        
+
+
+        String s[] = {"Denver","Kites","Hopier","Sholey"};
+
+        for(String k:s)
+        {
+            System.out.println("Value : "+k.toUpperCase());
+        }
+
+    }
+}
