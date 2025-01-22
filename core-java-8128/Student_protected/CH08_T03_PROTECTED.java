a38187f068f5cd3042086a9b7c68c108cccd1528
gkhell <gpkmyworld@gmail.com>
2025-01-22 14:33:05 +0530

    22/01/2025 14:32 Wednesday

---
+++ core-java-8128/Student_protected/CH08_T03_PROTECTED.java
@@ -0,0 +1,14 @@
+package Student_protected;
+import Student_protected.*;
+public class CH08_T03_PROTECTED {
+    protected void show()
+    {
+        System.out.println(this+" Hello I'm Protected!!");
+    }
+
+    public void display()
+    {
+        show();
+    }
+    
+}
---
+++ core-java-8128/ch08_protected/CH08_P03_Protected_Pkg.java
@@ -0,0 +1,13 @@
+
+package ch08_protected;
+
+public class CH08_P03_Protected_Pkg {
+
+    public void show() {
+        System.out.println("I am Iron Man, how are you !!");
+    }
+
+    protected void demo() {
+        System.out.println("I am protected.. ");
+    }
+}
