e15b145cd1bd417bdb3e532f416953c5ee10411c
gkhell <gpkmyworld@gmail.com>
2025-01-30 11:02:03 +0530

    30/01/2025 11:01 Thursday

--- mern-8788/ch04_working_with_state_props/src/components/ValidateUser.jsx
+++ mern-8788/ch04_working_with_state_props/src/components/ValidateUser.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ValidatedComponent from "./ValidatedComponent";
 
 const ValidateUser = (props) => {
-  return <div>Welcome, {props.username}</div>;
+  return (
+    <div>
+      Welcome, {props.username}
+      <ValidatedComponent />
+    </div>
+  );
 };
 
 ValidateUser.propTypes = {
   username: PropTypes.number.isRequired,
 };
 
-ValidateUser.defaultProps={
-  password:"kites"
-}
+ValidateUser.defaultProps = {
+  password: "kites",
+};
 
 export default ValidateUser;
