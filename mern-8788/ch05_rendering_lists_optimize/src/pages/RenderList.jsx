bc2fe912bbd511e4f4a8dbb1a89924daddbd4b1c
gkhell <gpkmyworld@gmail.com>
2025-02-01 10:36:06 +0530

    01/02/2025 10:35 Saturday

--- mern-8788/ch05_rendering_lists_optimize/src/pages/RenderList.jsx
+++ mern-8788/ch05_rendering_lists_optimize/src/pages/RenderList.jsx
@@ -5,33 +5,41 @@ function RenderList() {
 
   const [limit, setLimit] = useState(2);
   const [currentPage, setCurrentPage] = useState(1);
-  let arr = Array.from({ length: 20000 }, (element, index) => ({
+  let arr = Array.from({ length: 200000 }, (element, index) => ({
     url: `${url}${index}`,
     name: `My Image ${index}`,
   }));
 
-  
   const startIndex = (currentPage - 1) * limit;
   const endIndex = startIndex + parseInt(limit);
-  
+  const totalPages = Math.ceil(arr.length / limit);
   const currentItems = arr.slice(startIndex, endIndex);
 
+  const handleChangePage = (newPage) => {
+    setCurrentPage(newPage);
+
+    // auto scroll
+    const element = document.getElementById("my-page");
+    if (element) {
+      element.scrollIntoView("smooth");
+    }
+  };
   return (
-    <div className="container p-2">
+    <div className="container p-2"  id="my-page">
       <div>
         <div>
           <input
             value={limit}
             onChange={(e) => {
+              setCurrentPage(1)
               setLimit(e.target.value);
             }}
           />
-          {limit}
         </div>
         {currentItems.map((a, i) => (
           <div key={i}>
             <div className="d-flex align-items-center gap-3 mt-3 bg-secondary bg-opacity-50 p-2 rounded-3">
-              <div>{currentPage * limit + i + 1}</div>
+              <div>{(currentPage -1) * limit + i + 1}</div>
               <img
                 width={50}
                 height={50}
@@ -46,16 +54,19 @@ function RenderList() {
         <div className="d-flex mt-5 gap-5">
           <button
             className="btn btn-primary"
+            disabled={currentPage == 1}
             onClick={() => {
-              setCurrentPage((p) => p - 1);
+              handleChangePage(currentPage - 1);
             }}
           >
             privious
           </button>
+          {currentPage}/{totalPages}
           <button
             className="btn btn-primary"
+            disabled={currentPage == totalPages}
             onClick={() => {
-              setCurrentPage((p) => p + 1);
+              handleChangePage(currentPage + 1);
             }}
           >
             Next
