e296caaa90afefd07129f40834eb8f33fb6ecf49
gkhell <gpkmyworld@gmail.com>
2025-02-14 17:36:22 +0530

    14/02/2025 17:36 Friday

--- mern-1234/react-practice/src/App.js
+++ mern-1234/react-practice/src/App.js
@@ -1,22 +1,24 @@
-import react,{useState} from "react";
-import "./App.css";
+import react, { useState } from "react";
+//import "./App.css";
 function Home() {
-  // var name = "CEC";
-  const [name,setName] = useState("CEC");
+  // var name="mascev";
+  const [name, setName] = useState("CEC");
+  const [rollno, setRollNo] = useState(1);
+  const [marks, setMarks] = useState(100);
 
-
-  const handleClick = () => {
-    // name = "MASCEV";
-    setName("Mascev")
-    console.log("HEllo Coder ",name);
+  const handleCli = () => {
+    setName("goparsir");
+    setRollNo(2);
+    setMarks(200);
+    console.log({ name, rollno, marks });
   };
-
   return (
     <div className="App">
-      <h1>Hello World | {name}</h1>
-      <button onClick={handleClick}>Click Me</button>
+      <h1>
+        heading {name},{rollno},{marks}
+      </h1>
+      <button onClick={handleCli}>click me</button>
     </div>
   );
 }
-
 export default Home;
