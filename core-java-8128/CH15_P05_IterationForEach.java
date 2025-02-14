9e782daaa686873f3cb99a752aaac6831915e324
gkhell <gpkmyworld@gmail.com>
2025-02-14 18:27:12 +0530

    14/02/2025 18:27 Friday

---
+++ core-java-8128/CH15_P05_IterationForEach.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.util.List;
+
+public class CH15_P05_IterationForEach {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Set<String> data = new HashSet<>();
+
+        String text;
+        System.out.println("Type 'exit' for terminate : ");
+        while (true) {
+            System.out.print("Type : ");
+            text = sc.nextLine();
+            if (text.compareToIgnoreCase("exit") == 0) {
+                break;
+            }
+
+            data.add(text);
+            System.out.println("\n " + text + " added  +" + data.size());
+
+        }
+
+        /*  */
+        System.out.println("----------: for-each : --------------");
+        for (String s : data) {
+            System.out.println(" => " + s);
+        }
+
+        System.out.println("\n\n----------: Itertion : --------------");
+
+        Iterator<String> it = data.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(" => " + it.next());
+
+        }
+
+        System.out.println("\n\n----------:List Itertion : --------------");
+
+        List<String> list = new LinkedList<>();
+
+        list.addAll(data);
+
+        ListIterator<String> listIt = list.listIterator();
+
+        String key;
+        while (listIt.hasNext() || listIt.hasPrevious()) {
+
+            System.out.print("Enter Option : ");
+            key = sc.nextLine();
+
+            if (key == "p") {
+
+            }
+        }
+
+        sc.close();
+    }
+}
