d632ad07dc598e8d261b83295189033eef15a1b1
gkhell <gpkmyworld@gmail.com>
2025-02-15 11:54:19 +0530

    15/02/2025 11:54 Saturday : update react component

--- mern-1234/react-practice/src/App.js
+++ mern-1234/react-practice/src/App.js
@@ -1,24 +1,22 @@
-import react, { useState } from "react";
-//import "./App.css";
-function Home() {
-  // var name="mascev";
-  const [name, setName] = useState("CEC");
-  const [rollno, setRollNo] = useState(1);
-  const [marks, setMarks] = useState(100);
-
-  const handleCli = () => {
-    setName("goparsir");
-    setRollNo(2);
-    setMarks(200);
-    console.log({ name, rollno, marks });
-  };
+import React from "react";
+import AboutUs from "./componets/AboutUs";
+import ContactUs from "./componets/ContactUs";
+import Product from "./componets/Product";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function App() {
   return (
-    <div className="App">
-      <h1>
-        heading {name},{rollno},{marks}
-      </h1>
-      <button onClick={handleCli}>click me</button>
+    <div className="container bg-danger bg-opacity-25 p-2 rounded-3">
+      <h1 className="text-center">React Demo</h1>
+      <hr />
+      <div className="mb-5">
+        <AboutUs />
+      </div>
+      <div className="mb-5">
+        <Product />
+      </div>
+      <div className="mb-5">
+        <ContactUs />
+      </div>
     </div>
   );
 }
-export default Home;
