40ccf9f389184f43de38cc8930f1a2bc7fd28e44
gkhell <gpkmyworld@gmail.com>
2025-02-18 11:17:29 +0530

    18/02/2025 11:17 Tuesday

--- mern-1234/react-practice/src/App.js
+++ mern-1234/react-practice/src/App.js
@@ -3,11 +3,15 @@ import AboutUs from "./componets/AboutUs";
 import ContactUs from "./componets/ContactUs";
 import Product from "./componets/Product";
 import "bootstrap/dist/css/bootstrap.min.css";
+import InputStates from "./componets/InputStates";
 export default function App() {
   return (
     <div className="container bg-danger bg-opacity-25 p-2 rounded-3">
       <h1 className="text-center">React Demo</h1>
       <hr />
+      <div className="mb-5 bg-white p-2 rounded-3">
+        <InputStates />
+      </div>
       <div className="mb-5">
         <AboutUs />
       </div>
---
+++ mern-1234/react-practice/src/componets/StudentExample.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StudentExample() {
+  return (
+    <div>StudentExample</div>
+  )
+}
+
+export default StudentExample
