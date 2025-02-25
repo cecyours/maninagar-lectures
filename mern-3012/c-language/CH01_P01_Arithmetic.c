4f0034506a028ebdd1681031f1413ae84ea9b66a
gkhell <gpkmyworld@gmail.com>
2025-02-25 11:46:37 +0530

    25/02/2025 11:46 Tuesday

---
+++ mern-3012/c-language/CH01_P01_Arithmetic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+		int a,b,c;
+	clrscr();
+		printf("Enter the number : ");
+		scanf("%d",&a);
+
+		printf("Enter the number : ");
+		scanf("%d",&b);
+
+		c = a+b;
+		printf("%d + %d = %d\n",a,b,c);
+
+		c = a-b;
+		printf("%d - %d = %d\n",a,b,c);
+
+		// area = l*b;
+		// area = pi*r*r
+
+	getch();
+}  
