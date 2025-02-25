cbb275e3b0fb9f27cf389bd1790d38956b3f9d53
gkhell <gpkmyworld@gmail.com>
2025-02-25 11:55:40 +0530

    25/02/2025 11:55 Tuesday

---
+++ mern-3012/CH01_P02_area.c
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+	float area,PI=3.14,r;
+clrscr();
+	printf("Enter the radius : ");
+	scanf("%f",&r);
+
+	area = r*r*PI;
+
+	printf("Area is %.2f\n",area);
+
+
+getch();
+}  
