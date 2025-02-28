3f2ec3e7f6e329c5a2a89c9cd56e17d44bcf0fe7
gkhell <gpkmyworld@gmail.com>
2025-02-28 18:01:04 +0530

    28/02/2025 18:01 Friday

---
+++ mern-1234/ch02-templating-jsx/src/App.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import CreateElementDemo from "./page/CreateElementDemo";
+function App() {
+  const topics = [
+    {
+      url: "/create-element",
+      label: "Create Element",
+      component: <CreateElementDemo />,
+    },
+    {
+      url: "/expression",
+      label: "Expressions",
+      component: <>Hello Expressions</>,
+    },
+    {
+      url: "/logical-operators",
+      label: "logical operators",
+      component: <>Hello logical</>,
+    },
+    {
+      url: "/specifying-attributes",
+      label: "Specifying attributes",
+      component: <>Hello attributes</>,
+    },
+    {
+      url: "/specifying-children",
+      label: "Specifying children",
+      component: <>Hello children</>,
+    },
+    { url: "/fragments", label: "Fragments", component: <>Hello Fragments</> },
+  ];
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <div className="container">
+        <BrowserRouter>
+          <div className="row">
+            <div className="col-md-2">
+              {topics.map((topic, i) => (
+                <div key={i}>
+                  <Link to={topic.url} className="text-decoration-none border">
+                    {topic.label}
+                  </Link>
+                </div>
+              ))}
+            </div>
+            <div className="col-md-8">
+              <div className="border rounded-3">
+                <Routes>
+                  {topics.map((topic, i) => (
+                    <>
+                      <Route path={topic.url} element={topic.component}></Route>
+                    </>
+                  ))}
+                </Routes>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ mern-1234/ch02-templating-jsx/src/page/CreateElementDemo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateElementDemo() {
+  return (
+    <div>CreateElement Demo</div>
+  )
+}
+
+export default CreateElementDemo
