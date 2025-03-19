3d7718b4e0fdcc05a00d2ac6d76a5561333d7d0f
gkhell <gpkmyworld@gmail.com>
2025-03-19 18:35:57 +0530

    19/03/2025 18:35 Wednesday

--- core-java-8128/CH19_P08_StudentProject.java
+++ core-java-8128/CH19_P08_StudentProject.java
@@ -12,40 +12,72 @@ public class CH19_P08_StudentProject {
 }
 
 
-class Window extends JFrame{
+class Window extends JFrame {
 
-    JPanel windowPanel; // border layout
-    JPanel mainPanel; // border layout
+    JPanel windowPanel;
+    JPanel mainPanel;
     JPanel menuPanel;
     JPanel sidePanel;
+    JMenuBar menuBar;
 
-    public Window()
-    {
+    JLabel lblHeading;
+    JLabel lblUserName;
+    JTextField txtUserName;
+
+    JLabel lblFullName;
+    JLabel lblEmail;
+    JLabel lblPassword;
+
+    public Window() {
         windowPanel = new JPanel(new BorderLayout());
-        menuPanel = new JPanel(new FlowLayout());
-
-        menuPanel.setBackground(Color.RED);
+        menuPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        menuPanel.setBackground(new Color(23, 150, 20));
         sidePanel = new JPanel();
-        BoxLayout box = new BoxLayout(sidePanel,BoxLayout.Y_AXIS);
-        sidePanel.setLayout(box);
+        sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.Y_AXIS));
 
+        mainPanel = new JPanel(new GridBagLayout()); // ✅ Use GridBagLayout
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10); // spacing between components
 
-        mainPanel = new JPanel();
+        windowPanel.add(menuPanel, BorderLayout.NORTH);
+        windowPanel.add(sidePanel, BorderLayout.EAST);
+        windowPanel.add(mainPanel, BorderLayout.CENTER);
 
-        windowPanel.add(menuPanel,BorderLayout.NORTH);
-        windowPanel.add(sidePanel,BorderLayout.EAST);
-        windowPanel.add(mainPanel,BorderLayout.CENTER);
+        /* Init menu bar */
+        menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.add(new JMenuItem("Create a File"));
+        fileMenu.add(new JMenuItem("Save As"));
+        fileMenu.add(new JMenuItem("Rename File"));
+        menuBar.add(fileMenu);
+        menuBar.add(new JMenuItem("Home"));
+        menuBar.add(new JMenuItem("About"));
+        menuBar.add(new JMenuItem("Setting"));
+        menuPanel.add(menuBar);
 
+        /* Form Content */
+        lblHeading = new JLabel("Welcome to Register Page");
+        lblHeading.setFont(new Font("Arial", Font.BOLD, 16));
 
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        mainPanel.add(lblHeading, gbc);
 
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.WEST;
 
-        // add button in menu
+        lblUserName = new JLabel("Enter Username:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        mainPanel.add(lblUserName, gbc);
 
-        JButton btn = new JButton("Home");
-        menuPanel.add(btn);
-
+        txtUserName = new JTextField(20); // preferred width (20 columns)
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        mainPanel.add(txtUserName, gbc);
 
         getContentPane().add(windowPanel);
     }
-
 }
