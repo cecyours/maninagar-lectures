a529f0ae069d52ac6dc1b7f2dc6801cb7cdf34a1
vish0201 <rabariv832@gmail.com>
2025-04-19 12:02:50 +0530

    redux

--- mern-1234/ch05-react-router/src/App.js
+++ mern-1234/ch05-react-router/src/App.js
@@ -9,6 +9,7 @@ import UserProfile from './pages/UserProfile';
 import Dashboard from './pages/admin-panel/Dashboard';
 import Users from './pages/admin-panel/Users';
 import Setting from './pages/admin-panel/Setting';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -16,15 +17,17 @@ function App() {
       <Router>
         <NavbarComponents />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home user={"Vishal"} />} />
           <Route path='/users' element={<User />} />
           <Route path='/users/:id' element={<UserProfile />} />
           {/* Routes  for admin panel */}
           <Route path='/admin' element={<Dashboard />} />
           <Route path='/admin/users' element={<Users />} />
           <Route path='/admin/settings' element={<Setting />} />
-          
-          
+          {/* FOr path less route */}
+          <Route path='*' element={<NotFound />} />
+
+
         </Routes>
       </Router>
     </div>
--- mern-1234/ch05-react-router/src/pages/Home.jsx
+++ mern-1234/ch05-react-router/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Home = () => {
+const Home = ({ user }) => {
   return (
     <div>
-      <h1>This is home page</h1>
+      <h1>Logged in user id {user}</h1>
+
+      <h1>This is home page </h1>
     </div>
   );
 };
