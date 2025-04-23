b2a645658179f003357a4c16f2cd0e6db57d5019
vish0201 <rabariv832@gmail.com>
2025-04-23 17:09:42 +0530

    redux middleware

--- mern-1234/ch06-react-redux/src/sagas/counterSagas.js
+++ mern-1234/ch06-react-redux/src/sagas/counterSagas.js
@@ -1,3 +1,5 @@
+import { increment } from "../redux/counterSlice";
+
 const { delay, put, takeEvery } = require("redux-saga/effects");
 
 const INCREMENT = "INCREMENT";
@@ -7,12 +9,18 @@ const DECREMENT_ASYNC = "DECREMENT_ASYNC"
 
 function* incrementAsync() {
     yield delay(1000)
-    yield put({ type: INCREMENT })
+    // yield put({ type: INCREMENT })
+    // we have done mistake in above line the correnct version in below so comment above line
+    yield put(increment())
+
 }
 
 function* decrementAsync() {
     yield delay(1000);
-    yield put({ type: DECREMENT })
+    // yield put({ type: DECREMENT })
+    // we have done mistake in above line the correnct version in below so comment above line
+    yield put(increment())
+
 }
 
 function* watchCounterSaga() {
