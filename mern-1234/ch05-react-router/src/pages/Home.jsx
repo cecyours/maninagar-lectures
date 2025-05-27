7b52a6cbf10596c5952367f91f4da95b3b866d91
vish0201 <rabariv832@gmail.com>
2025-05-27 18:18:01 +0530

    done

--- mern-1234/ch05-react-router/src/pages/Home.jsx
+++ mern-1234/ch05-react-router/src/pages/Home.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-const Home = ({ user }) => {
+const Home = ({ userprops }) => {
+  const [user, setUser] = useState([]);
   useEffect(() => {
     async function fetchUser() {
-      const user = await fetch("http://localhost:9000/users");
-      console.log(await user.json());
+      const user = await fetch(`http://localhost:9000/users `);
+      const userData = await user.json();
+      setUser(userData);
     }
-
     fetchUser();
   }, []);
 
   return (
     <div>
       <h1 className="text-center fs-3 mt-3 text-uppercase">
-        Logged in user id {user}
+        Logged in user id {userprops}
       </h1>
+
+      <div className="container">
+        {user.map((user) => (
+          <>
+            <li>{user.id}</li>
+            <li>{user.name}</li>
+            <li>{user.age}</li>
+          </>
+        ))}
+      </div>
     </div>
   );
 };
