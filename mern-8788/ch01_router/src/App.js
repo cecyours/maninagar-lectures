bd7421b49235f4b5e629ea2159a29764ad8ae1fc
vish0201 <rabariv832@gmail.com>
2025-06-11 18:14:45 +0530

    uplolod file

--- mern-8788/ch01_router/src/App.js
+++ mern-8788/ch01_router/src/App.js
@@ -13,3 +13,48 @@ function MyApp() {
 }
 
 export default MyApp;
+
+
+
+// src/utils/uploadFile.js
+
+// import axiosInstance from "@/app/config/axiosInstance";
+// import { textToSlug } from "./textUtils";
+
+// const uploadFile = async (
+//   file: File,
+//   folder: string,
+//   endpoint: string,
+//   userPrefix: string
+// ) => {
+//   try {
+//     userPrefix = textToSlug(userPrefix);
+//     if (!file) {
+//       return { success: false, error: "File upload failed" };
+//     }
+
+//     const formData = new FormData();
+//     formData.append("file", file);
+
+//     const uploadEndpoint = `${endpoint}?prefix=${encodeURIComponent(
+//       userPrefix
+//     )}&folder=${encodeURIComponent(folder)}`;
+
+//     const response = await axiosInstance.post(uploadEndpoint, formData, {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//     });
+
+//     if (response.data.success) {
+//       return { success: true, fileName: response.data.fileName };
+//     } else {
+//       return { success: false, error: "File upload failed" };
+//     }
+//   } catch (error) {
+//     console.error("Error uploading file:", error);
+//     return { success: false, error: "An error occurred during file upload" };
+//   }
+// };
+
+// export default uploadFile;
