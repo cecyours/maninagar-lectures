d7fdea5a54320155b101185877bd00031a9c68e6
vish0201 <rabariv832@gmail.com>
2025-08-05 09:59:24 +0530

    javascript

---
+++ MERN-1323/JS/variable.js
@@ -0,0 +1,25 @@
+// variable in js
+
+
+var company = 1
+
+var company = "mascev private limited"
+
+var course = "Developer"
+
+
+
+let fullName = "rosewin"
+
+
+fullName = "vish"
+
+
+console.log(fullName);
+
+const PI = 3.14
+
+
+PI = 5.14
+
+console.log(PI);
