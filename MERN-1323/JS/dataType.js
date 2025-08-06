f46da9769eb0bce920be22529e13ada0433a798f
vish0201 <rabariv832@gmail.com>
2025-08-06 10:30:58 +0530

    Javascript

---
+++ MERN-1323/JS/dataType.js
@@ -0,0 +1,25 @@
+let number = 6
+// print datatype of number
+console.log(typeof number);
+
+// -----------------------
+
+let carName = 'mini cooper'
+// print datatype of  carName
+console.log(typeof carName);
+
+// -----------------------
+
+
+// boolean
+let isVote = true
+console.log(typeof isVote);
+
+
+let known = null
+console.log(typeof known);
+
+
+
+
+
--- MERN-1323/JS/variable.js
+++ MERN-1323/JS/variable.js
@@ -20,6 +20,17 @@ console.log(fullName);
 const PI = 3.14
 
 
-PI = 5.14
+// PI = 5.14
 
 console.log(PI);
+
+
+// rules of variable
+
+let y = 7
+let Y = 6
+
+console.log(y);   // y and Y is trate as diffrent
+
+console.log(Y);
+
