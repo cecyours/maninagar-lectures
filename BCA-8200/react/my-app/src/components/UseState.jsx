ada9216881784a49ab2ca327a3221ed8f00f85c6
vish0201 <rabariv832@gmail.com>
2025-08-08 13:28:53 +0530

    done

---
+++ BCA-8200/react/my-app/src/components/UseState.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function UseState() {
+  const [counter, setCounter] = useState(0);
+
+  const handleMinusClick = () => {
+    if (counter <= 0) return;
+
+    setCounter(counter - 1);
+  };
+
+  const handlePlusClick = () => {
+    setCounter(counter + 1);
+  };
+  return (
+    <div>
+      <h1>Use State in react</h1>
+
+      <button onClick={handleMinusClick} className="btn btn-secondary me-5">
+        -
+      </button>
+      <div className="badge text-bg-danger fs-3"> {counter} </div>
+      <button onClick={handlePlusClick} className="btn btn-secondary ms-5">
+        +
+      </button>
+
+      {"🎉".repeat(counter)}
+    </div>
+  );
+}
+
+export default UseState;
