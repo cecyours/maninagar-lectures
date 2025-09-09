eba23c833e5dd8b47dfa5c182a6811aa6d59ed70
master <gpkmyworld@gmail.com>
2025-09-09 17:53:20 +0530

    09/09/2025 ⎦⎡ 17:53 ⎦⎢ Tuesday

---
+++ python-3010/CH01_P01_arithmetic.py
@@ -0,0 +1,26 @@
+
+a = int(input("Enter the number : ")) # 5
+b = int(input("Enter the number : ")) # 2
+
+c = a+b # 7
+print(a,"+",b,"=",c)
+
+c = a-b # 3
+print(a,"-",b,"=",c)
+
+c = a*b
+print(a,"x",b,"=",c)
+
+c = a/b
+print(a,"/",b,"=",c)
+
+c = a//b
+print(a,"//",b,"=",c)
+
+c = a**b
+print(a,"**",b,"=",c)
+
+
+
+
+
---
+++ python-3010/main.py
@@ -0,0 +1,14 @@
+
+print("Apple","Banana","Cherry",sep=',',end=".\n")
+
+print("Hello World")
+
+# this is comment
+
+fname = "Roy"
+lname = "Miet"
+
+fullname = fname+" "+lname
+
+print(fullname)
+
