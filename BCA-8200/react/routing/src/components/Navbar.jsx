b417be437bf6b199e69233d7c7172397e9eaa137
vish0201 <rabariv832@gmail.com>
2025-09-10 12:38:21 +0530

    done code

--- BCA-8200/react/routing/src/components/Navbar.jsx
+++ BCA-8200/react/routing/src/components/Navbar.jsx
@@ -52,14 +52,13 @@ const Navbar = () => {
           </ul>
         </div>
 
+        
+
         {/* Right Side Username */}
         <div className="d-flex flex-grow-1 justify-content-end  align-items-center gap-2">
           <button
             type="button"
             class="btn btn-sm btn-primary position-relative"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#staticBackdrop"
-            aria-controls="staticBackdrop"
           >
             Cart
             <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
@@ -67,29 +66,6 @@ const Navbar = () => {
             </span>
           </button>
           <span className="navbar-text fw-semibold">Hello, {username}</span>
-        </div>
-
-        <div
-          className="offcanvas offcanvas-start"
-          data-bs-backdrop="static"
-          tabIndex="-1"
-          id="staticBackdrop"
-          aria-labelledby="staticBackdropLabel"
-        >
-          <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="staticBackdropLabel">
-              Offcanvas
-            </h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body">
-            <div>I will not close if you click outside of me.</div>
-          </div>
         </div>
       </div>
     </nav>
