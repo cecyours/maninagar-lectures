98e25bb2dd4e075714b3bbfd81445026500163f8
tanvidarji642 <darjitanvi642@gmail.com>
2025-10-03 15:33:28 +0530

    Relational

---
+++ c-language-6480/operatorsINC/arithmetic/relational.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+    int a,b;
+
+    printf("Enter the 2 Numbers: ");
+    scanf("%d, %d", &a, &b);
+
+    printf("%d > %d : %d\n", a, b, a > b);
+    printf("%d < %d : %d\n", a, b, a < b);
+    printf("%d >= %d : %d\n", a, b, a >= b);
+    printf("%d <= %d : %d\n", a, b, a <= b);
+    printf("%d != %d : %d\n", a, b, a != b);
+    printf("%d == %d : %d\n", a, b, a == b);
+
+}
