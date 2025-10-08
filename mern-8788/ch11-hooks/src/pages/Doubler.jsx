454b05be5b6854a5cc9976323a22f0cd2dbb49b5
tanvidarji642 <darjitanvi642@gmail.com>
2025-10-08 10:14:52 +0530

    Doubler

--- mern-8788/ch11-hooks/src/pages/Doubler.jsx
+++ mern-8788/ch11-hooks/src/pages/Doubler.jsx
@@ -5,7 +5,7 @@ function Doubler() {
  
   return ( 
     <div style={{ textAlign: "center", marginTop: "50px" }}> 
-      <h1>      Number Doubler</h1> 
+      <h1>Number Doubler</h1> 
       <h2>Value: {num}</h2> 
       <button onClick={() => setNum(num * 2)}>Double It</button> 
       <button onClick={() => setNum(1)}>Reset</button> 
