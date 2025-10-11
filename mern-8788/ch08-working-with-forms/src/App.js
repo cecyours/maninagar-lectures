1c17355d298100113c55563883aff0f533b9332e
tanvidarji642 <darjitanvi642@gmail.com>
2025-10-11 17:49:26 +0530

    2-D Array

--- mern-8788/ch08-working-with-forms/src/App.js
+++ mern-8788/ch08-working-with-forms/src/App.js
@@ -5,6 +5,7 @@ import Student from "./components/Student";
 import ControllInput from "./components/ControllInput";
 import UncontrolledInput from "./components/UncontrolledInput";
 import SignupForm from "./components/SignupForm";
+import ContactForm from "./components/ContactForm";
 
 
 function App() {
@@ -17,7 +18,8 @@ function App() {
     // </div>
     // <ControllInput/>
     // <UncontrolledInput/>
-    <SignupForm/>
+    // <SignupForm/>
+    <ContactForm/>
   );
 }
 
