9d48f9a4440d89b1528204f6c44a90f9f9285563
tanvidarji642 <darjitanvi642@gmail.com>
2025-10-27 17:31:29 +0530

    Hello World

---
+++ MERN-1323/C_Lang/Function/HelloWorld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void display(){
+    printf("Hello World ");
+    printf("Hello Im Tanvi \n");
+}
+
+void main(){
+    display();
+    display();
+    display();
+    display();
+    display();
+}
