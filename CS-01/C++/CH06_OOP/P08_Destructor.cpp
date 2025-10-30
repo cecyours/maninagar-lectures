497b245c276bf09f969dc4440ffb6acf305b0f69
tanvidarji642 <darjitanvi642@gmail.com>
2025-10-30 18:07:02 +0530

    Destructor

---
+++ CS-01/C++/CH06_OOP/P08_Destructor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class Student {
+public:
+    string name;
+
+    Student(string n) {
+        name = n;
+        cout << "Constructor called for " << name << endl;
+    }
+   
+    ~Student() {
+        cout << "Destructor called for " << name << endl;
+    }
+    void display() {
+        cout << "Name: " << name << endl;
+    }
+};
+int main() {
+    Student s1("Tanvi");  
+    s1.display();
+    Student s2("Aryan");  
+    s2.display();
+    return 0;
+}
