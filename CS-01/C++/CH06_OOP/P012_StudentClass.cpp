8875f17b0d5b22e177f0194e285b038eb7ec504c
tanvidarji642 <darjitanvi642@gmail.com>
2025-10-31 17:27:53 +0530

    Student

---
+++ CS-01/C++/CH06_OOP/P012_StudentClass.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    string name;
+    int age;
+
+public:
+
+    void setName(string n) { name = n; }
+    void setAge(int a) { 
+        if(a > 0) 
+            age = a;
+        else 
+            cout << "Invalid age!" << endl;
+    }
+
+    string getName() { return name; }
+    int getAge() { return age; }
+};
+
+int main() {
+    Student s1;
+    s1.setName("Tanvi");
+    s1.setAge(22);
+    cout << "Student Name: " << s1.getName() << endl;
+    cout << "Student Age: " << s1.getAge() << endl;
+    return 0;
+}
