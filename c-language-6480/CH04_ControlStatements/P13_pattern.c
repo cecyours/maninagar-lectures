63caaaa56321c04bcbf68cfe4587b1162fb804a5
tanvidarji642 <darjitanvi642@gmail.com>
2025-11-06 11:03:02 +0530

    pattern

---
+++ c-language-6480/CH04_ControlStatements/P13_pattern.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+    int n, i, j;
+
+    printf("Enter the Size: ");
+    scanf("%d", &n);
+
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n; j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    
+}
