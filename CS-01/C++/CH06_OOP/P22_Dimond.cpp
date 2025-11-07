2d1dd8c2a8954ce175c488252acbcb80974b1ee5
tanvidarji642 <darjitanvi642@gmail.com>
2025-11-07 18:17:57 +0530

    Hybrid Inheritance

---
+++ CS-01/C++/CH06_OOP/P22_Dimond.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+class A {
+    public:
+    void show() { cout << "Class A" << endl; }
+};
+
+class B : public A {};
+class C : public A {};
+
+class D: public B, public C{};
+
+int main(){
+    D obj;
+
+    // obj.show();
+    obj.B::show();
+    obj.C::show();
+
+    return 0;
+}
