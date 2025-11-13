dc30a3a1b42c09c3cbd38989f871f3de677d0e79
tanvidarji642 <darjitanvi642@gmail.com>
2025-11-13 11:03:27 +0530

    module done

---
+++ mern-8788/ch14-node-js/index.js
@@ -0,0 +1,14 @@
+// const aryan = require('./math');
+
+// console.log(aryan.add(1,2));
+// console.log(aryan.sub(10,5));
+// console.log(aryan.multi(5,5));
+// console.log(aryan.div(8,2));
+
+
+const { add, sub, multi, div } = require('./math');
+
+console.log(add(1,2));
+console.log(sub(10,5));
+console.log(multi(5,5));
+console.log(div(8,2));
