f9158557b8b17ad0accaad2d4e2dae04da37e3cb
vish0201 <rabariv832@gmail.com>
2025-11-21 15:04:28 +0530

    category crud

--- BCA-8200/expressjs/backend/routes/category.js
+++ BCA-8200/expressjs/backend/routes/category.js
@@ -2,8 +2,14 @@ var express = require('express');
 const { default: Category } = require('../models/categoty');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-
+router.get('/', async function (req, res, next) {
+    try {
+        const categories = await Category.find()
+        res.status(200).json({ message: "Category fetched Successfully", data: categories })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Category fetching faied", error })
+    }
 });
 
 
