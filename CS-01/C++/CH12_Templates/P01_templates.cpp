cbcf43a2ec9e3c63b7aad066c5edd1aee30b1b39
tanvidarji642 <darjitanvi642@gmail.com>
2025-11-26 12:09:10 +0530

    Templates

---
+++ CS-01/C++/CH12_Templates/P01_templates.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+template <typename T>
+T maxi(T a, T b){
+    return a>b?a:b;
+} 
+
+int main(){
+    cout<<"Max from "<<maxi<double>(2.5,5.20)<<endl;
+    cout<<"Max from "<<maxi<double>(35.42,20)<<endl;
+}
