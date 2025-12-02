00dcb54c019ef8a884218145fb62376e0a3c86fc
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-02 10:56:15 +0530

    PUT

--- mern-8788/ch15_express/myserver/routes/dbuser.js
+++ mern-8788/ch15_express/myserver/routes/dbuser.js
@@ -35,6 +35,20 @@ router.post('/', async function (req, res, next) {
         res.status(500).json({ message: "failed to create user", error: error })
     }
     
-})
+});
+
+// Put for update user
+router.put('/:id', async function (req, res, next) {
+    try{
+        const { id } = req.params
+        const updateUserData = req.body
+
+        const user =  await User.findByIdAndUpdate(id, updateUserData, { new: true })
+        res.status(200).json({ message: "User Update Successfully", user: user })
+    }catch{
+        console.log(error);
+        res.status(500).json({ message: "failed to update user", error: error }) 
+    }
+});
 
 module.exports = router;
