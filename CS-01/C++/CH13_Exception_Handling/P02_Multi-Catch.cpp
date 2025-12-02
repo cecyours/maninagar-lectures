a27f9e2a6d9027dfdda688347a347201d14122d5
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-02 11:31:05 +0530

    try-catch

---
+++ CS-01/C++/CH13_Exception_Handling/P02_Multi-Catch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        int x;
+        cout << "Enter a number (0, 1, or any other): ";
+        cin >> x;
+        if (x == 0)
+            throw 0; 
+        else if (x == 1)
+            throw 1.5; 
+        else
+            throw "Unknown input"; 
+    }
+    catch (int e)
+    {
+        cout << "Caught an integer exception!" << endl;
+    }
+    catch (double e)
+    {
+        cout << "Caught a double exception!" << endl;
+    }
+    catch (const char *e)
+    {
+        cout << "Caught a string exception: " << e << endl;
+    }
+    return 0;
+}
