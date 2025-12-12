90b273445a6468e3e15502499edcbff7a0712e4c
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-12 11:44:24 +0530

    done

---
+++ CS-01/js/dataType.js
@@ -0,0 +1,33 @@
+let number = 6;
+// print datatype of number
+console.log(typeof number);
+
+// -----------------------
+
+let carName = "Harshpreet";
+// print datatype of carName
+console.log(typeof carName);
+
+// -----------------------
+
+// boolean
+let isVote = true;
+console.log(typeof isVote);
+
+// -----------------------
+
+let known = null;
+console.log(typeof known); // output: "object" (JavaScript ka historic bug)
+
+// -----------------------
+
+// Undefined
+let value;
+console.log(typeof value);
+
+// -----------------------
+
+// BigInt
+let bigNumber = 1132131232323121232322342343434432434344n
+console.log(typeof bigNumber);
+
