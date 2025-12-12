45762eca3813bb2c8f87f0d9c0977c23de6b48a2
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-12 17:28:55 +0530

    Done

--- MERN-1323/react/my-react-app/src/App.jsx
+++ MERN-1323/react/my-react-app/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import MComp from './components/ClassBased'
 import Footer from './components/Footer'
+import FunctionalBased from './components/FunctionalBased'
 import Navbar from './components/Navbar'
+import UserCard from './components/UserCard'
 
 function App() {
   return (
@@ -9,7 +11,12 @@ function App() {
       <div className="App">
         <Navbar />
 
-        <MComp /> 
+        {/* <MComp />  */}
+
+        {/* <FunctionalBased /> */}
+        
+        <UserCard name={"Rosewin"} age={18} /> 
+        <UserCard name={"Raju"} age={45} /> 
 
         
         <Footer />
