3e5f66d5f178948442f7a83065c5d9e3b8d14fee
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-15 17:53:41 +0530

    basic

---
+++ MERN-1323/react/basic/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import Button from './components/Button'
+import Student from './components/Student'
+import StudentList from './components/StudentList'
+
+function App() {
+
+  const Students = [
+    { roll:1, name: "Tanvi", course: "MERN Stack"},
+    { roll:2, name: "Rosewin", course: "MERN Stack"},
+    { roll:3, name: "Raju", course: "Full Stack"},
+    { roll:4, name: "Rajesh", course: "Full Stack"},
+    { roll:5, name: "Rajendra", course: "MERN Stack"},
+    { roll:6, name: "Rajju", course: "MERN Stack"}
+  ]
+
+
+  return (
+    <>
+      <div>
+
+        {/* <Student stuRoll={67} stuName={'Rosewin'} /> */}
+
+        {/* <Student stuRoll={56} stuName={'Tanvi'} /> */}
+
+        {/* <Student  />  */}
+
+        {/* <StudentList students={Students} /> */}
+
+        <Button />
+      </div>
+    </>
+  )
+}
+
+export default App
