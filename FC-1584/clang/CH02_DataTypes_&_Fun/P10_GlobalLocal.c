9a8028513b15cc5d653266965e53cb5b914d5f4f
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-18 10:54:44 +0530

    scanf

---
+++ FC-1584/clang/CH02_DataTypes_&_Fun/P10_GlobalLocal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int i;
+
+void main(){
+    int j;
+
+    printf("default value at local scope: %d\n", j);
+    printf("default value at global scope: %d\n", i);
+}
---
+++ FC-1584/clang/CH02_DataTypes_&_Fun/P11_StorageClass.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int e;
+extern int e;
+static int s;
+
+void main(){
+    auto int a;
+    register int r;
+
+    printf("auto : %d\n", a);
+    printf("register : %d\n", r);
+    printf("extern : %d\n", e);
+    printf("static : %d\n", s);
+}
