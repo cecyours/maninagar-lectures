2deff0b52efdf87cfac2e2e7f87d7043e9425ed1
Tanvi Darji <darjitanvi642@gmail.com>
2025-12-27 10:54:14 +0530

    specifiers

---
+++ MERN-2007/clang/Ch02_DataType/P02_datatype.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    char ch;
+    int size;
+    
+    size = sizeof(ch);
+
+    printf("size of character : %d byte\n", size);
+    
+    size = sizeof(int); 
+
+    printf("  size of integer : %d byte\n", size);
+   
+}
