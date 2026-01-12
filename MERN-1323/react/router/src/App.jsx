778e600f583f862b3b2c50f7eae1597e530842de
Tanvi Darji <darjitanvi642@gmail.com>
2026-01-12 17:47:06 +0530

    useeffect

---
+++ MERN-1323/react/router/src/App.jsx
@@ -0,0 +1,43 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setTodos(json))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+          {todos.map((t, index) => (
+            <div>
+              {t.title}
+              < hr />
+            </div>
+          ))}
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
