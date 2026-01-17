be6d69a2bf40e99431b5a89411c9b5a5454ed769
Tanvi Darji <darjitanvi642@gmail.com>
2026-01-17 15:46:06 +0530

    assignment

---
+++ mern-9773/clang/CH03/P10_assignment.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b;
+   
+    printf("Enter 2 numbers : ");
+    scanf("%d,%d", &a, &b);
+
+    a += b; // a = a+b;
+    printf("value of a after += : %d\n", a);
+
+    a *= b; // a = a*b;
+    printf("value of a after *= : %d\n", a);
+    a <<= b; // a = a<<b;
+    printf("value of a after <<= : %d\n", a);
+    a >>= b; // a = a&b;
+    printf("value of a after >>= : %d\n", a);
+
+    a %= 10; // a = a%10;
+    printf("value of a after %%= : %d\n", a);
+   
+}
