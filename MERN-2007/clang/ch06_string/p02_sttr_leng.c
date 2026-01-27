3405aaeb2760dcac95a24a9fe041f27b4bc802ea
Tanvi Darji <darjitanvi642@gmail.com>
2026-01-27 18:10:07 +0530

    length

---
+++ MERN-2007/clang/ch06_string/p02_sttr_leng.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char name[100];
+    int len;
+    
+    printf("Enter name : ");
+    gets(name);
+    len = strlen(name);
+    printf("length of %s is %d.\n", name, len);
+    
+}
