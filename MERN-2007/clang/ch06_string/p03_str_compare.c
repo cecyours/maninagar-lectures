ceb3e2ce9bd5b36e3a3be777a62a63f984e16fb5
Tanvi Darji <darjitanvi642@gmail.com>
2026-01-28 17:14:20 +0530

    done

---
+++ MERN-2007/clang/ch06_string/p03_str_compare.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char password[10] = "ABC";
+    char input[10];
+    int status;
+
+    printf("Enter password : ");
+    gets(input);
+    status = strcmp(input, password);
+    printf("status : %d\n", status);
+
+    if (!status)
+    {
+        printf("Login succeed ;) \n");
+    }
+    else
+    {
+        printf("login failed.. \n");
+    }
+   
+}
