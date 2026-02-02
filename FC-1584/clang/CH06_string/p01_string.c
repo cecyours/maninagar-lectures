fc2947ca45c6d23af2204d84cf34bf074ba154e8
Tanvi Darji <darjitanvi642@gmail.com>
2026-02-02 10:55:48 +0530

    Done

---
+++ FC-1584/clang/CH06_string/p01_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main()
+{
+    char str1[] = "Computer";                        
+    char str2[5] = {'l', 'i', 'f', 'e'};             
+    char str3[4] = {'H', 'o', 'm', 'e'};             
+    char str4[10] = {'w', 'o', 'r', '\0', 'l', 'd'}; 
+
+    printf("str1 : %s\n", str1);
+    printf("str2 : %s\n", str2);
+    printf("str3 : %s\n", str3);
+    printf("str4 : %s\n", str4);
+
+}
