d38cabadc18d37744dfe130b102089d0d688f59d
Tanvi Darji <darjitanvi642@gmail.com>
2026-02-12 17:12:53 +0530

    function

---
+++ MERN-2007/clang/ch07_functions/p06_with_argu_with_value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
+void main()
+{
+    int x, y, z;
+   
+    printf("Enter values as a+b : ");
+    scanf("%d+%d", &x, &y);
+   
+    z = sum(x, y); // x → a, y → b, c → z
+    printf("%d + %d = %d\n", x, y, z);
+   
+}
