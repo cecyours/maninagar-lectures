11651bfbebb6f951fd1fe9cdb284c42d5b2e04f6
Tanvi Darji <darjitanvi642@gmail.com>
2026-02-13 15:16:14 +0530

    string

---
+++ mern-9773/clang/CH06/p02_runtime.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    char str1[100];
+    char str2[100];
+    
+    printf("Enter the str1 : ");
+    scanf("%s", str1); // no need of & due to string.
+   
+    printf("Enter the str2 : ");
+    scanf("%s", str2); 
+    
+    printf("\n ------------- \n");
+    printf("str1 : %s\n", str1);
+    printf("str2 : %s\n", str2);
+
+}
