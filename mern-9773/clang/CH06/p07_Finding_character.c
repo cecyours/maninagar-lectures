766e0459cce7ca7790b581a817612b88847463a0
Tanvi Darji <darjitanvi642@gmail.com>
2026-02-16 15:50:25 +0530

    find char

---
+++ mern-9773/clang/CH06/p07_Finding_character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char str[100], ch = 'q', *pos;
+    
+    printf("Enter a string : ");
+    gets(str);
+    pos = strchr(str, ch);
+    if (pos)
+        printf(" '%c' is found at address %p\n", ch, pos);
+    else
+        printf(" '%c' is not found.\n", ch);
+    
+}
