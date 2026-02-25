a8b8c30cd24abd1881b3826c6a93445b4de9aea7
Tanvi Darji <darjitanvi642@gmail.com>
2026-02-25 15:24:28 +0530

    string

---
+++ c-language-6480/CH06_string/p02_run_time.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    char str1[100];
+    char str2[115];
+
+    printf("Enter the str1 : ");
+    scanf("%s", str1);
+
+    printf("Enter the str2 : ");
+    scanf("%s", str2);
+
+    printf("\n--------------------\n");
+    printf("Str1 : %s\n", str1);
+    printf("Str2 : %s\n", str2);
+    
+}
