ddf7e3c615d42b582428d6ea794445f80cc1a5ff
Tanvi Darji <darjitanvi642@gmail.com>
2026-02-26 16:21:36 +0530

    copy

---
+++ c-language-6480/CH06_string/p05_copy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+    char str1[100], str2[100];
+
+    printf("Enter a string : ");
+    scanf0("%s" , str1);
+    strcpy(str2, str1); // str2 ← str1
+    printf("str1 : %s\n", str1);
+    printf("str2 : %s\n", str2);
+    
+}
