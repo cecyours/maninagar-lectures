1a1b2f326348804a3022eba441fa35bf10e4dc24
Tanvi Darji <darjitanvi642@gmail.com>
2026-03-10 17:47:08 +0530

    Done

--- MERN-1323/express/myserver/routes/welcome.js
+++ MERN-1323/express/myserver/routes/welcome.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello I`m Rosewin!');
+  res.send({ name: "Hello", key: "World" });
 });
 
 module.exports = router;
