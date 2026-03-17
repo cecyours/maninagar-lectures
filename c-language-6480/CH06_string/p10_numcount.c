89197e5a107f5698fcd85a376d7e9a018207cd42
Tanvi Darji <darjitanvi642@gmail.com>
2026-03-17 10:57:07 +0530

    Done

---
+++ c-language-6480/CH06_string/p10_numcount.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i=0; str[i] != '\0'; i++) {
+        if(str[i] >= '0' && str[i] <= '9') {
+            count++;
+        }
+    }
+
+    printf("Total digits = %d", count);
+
+    return 0;
+}
