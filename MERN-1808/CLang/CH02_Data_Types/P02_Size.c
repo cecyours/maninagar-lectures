489d8c40a851344e49e0f3f65c96b4b1a46a0b82
Tanvi Darji <darjitanvi642@gmail.com>
2026-04-03 12:44:18 +0530

    Done

---
+++ MERN-1808/CLang/CH02_Data_Types/P02_Size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    char ch;
+    int size;
+
+    size = sizeof(ch);
+    printf("Size of Character: %d byte\n", size);
+
+    size = sizeof(int);
+    printf("Size of integer : %d byte\n", size);
+    
+}
