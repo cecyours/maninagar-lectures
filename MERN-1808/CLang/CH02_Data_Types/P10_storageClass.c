7327d99491578a127044511fac8e72508936c5bc
Tanvi Darji <darjitanvi642@gmail.com>
2026-04-10 12:16:06 +0530

    Storage classes

---
+++ MERN-1808/CLang/CH02_Data_Types/P10_storageClass.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int e;
+extern int e;
+static int s;
+
+void main(){
+  
+    auto int a;
+    register int r;
+
+    printf("Extern: %d\n", e);
+    printf("static: %d\n", s);
+    printf("auto: %d\n", a);
+    printf("register: %d\n", r);
+
+}
