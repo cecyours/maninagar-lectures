b53d40f41139f52f52e6f8c59e29e05d3f413d1b
Tanvi Darji <darjitanvi642@gmail.com>
2026-04-13 11:50:11 +0530

    Done

---
+++ DA-6305/python/P04_Indentation.py
@@ -0,0 +1,25 @@
+# Var = 5
+# var = 10
+# print(Var)
+# print(var)
+
+# Statement
+x = 10  # assignment statement
+
+# Expression
+y = x + 5
+print(y)
+
+a = 5; b = 10; print(a + b)
+
+num = 10
+
+if num > 0:
+    print("Number is positive")
+    print("This is inside the if block")
+print("This is outside the if block")
+
+for i in range(1, 4):
+    print("Iteration:", i)
+    print("Inside the loop")
+print("Loop ended")
