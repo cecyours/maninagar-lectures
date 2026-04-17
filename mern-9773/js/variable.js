ac2450a66c985f475cacf80f64867087d38f499c
Tanvi Darji <darjitanvi642@gmail.com>
2026-04-17 16:23:19 +0530

    variable

---
+++ mern-9773/js/variable.js
@@ -0,0 +1,21 @@
+// updated
+var company = 1;
+var company = "Mascev Private Limited"; 
+
+var course = "Developer";
+
+let fullName = "wdawtanvis";
+fullName = "rrrr"; // updated value
+
+console.log("Full Name:", fullName);
+
+const PI = 3.14;
+// PI = 5.14; const value cannot change
+
+console.log("PI Value:", PI);
+
+let y = 7;
+let Y = 6; 
+
+console.log("Value of y:", y); 
+console.log("Value of Y:", Y);
