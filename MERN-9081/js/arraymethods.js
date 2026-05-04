c937cc142d15a1c2a4d76bc8762ae90257ae4391
Tanvi Darji <darjitanvi642@gmail.com>
2026-05-04 11:06:17 +0530

    Done

---
+++ MERN-9081/js/arraymethods.js
@@ -0,0 +1,10 @@
+const fruits = ["kiwi", 'Apple', "kiwi", "Banana", "kiwi"]
+
+console.log(fruits.lastIndexOf('kiwi'));
+
+fruits.unshift("Rose")
+
+fruits.pop()
+
+console.log(fruits);
+
---
+++ MERN-9081/js/lifetime.js
@@ -0,0 +1,26 @@
+
+function helloUser() {
+    var username = "Hello"
+    console.log("inner", username);
+}
+
+var fullName = "Nasrin Banu"
+
+function helloVikash() {
+
+console.log(fullName);
+
+}
+
+console.log(fullName);
+
+
+
+
+helloUser()
+
+
+
+
+console.log("Outer", username);
+
