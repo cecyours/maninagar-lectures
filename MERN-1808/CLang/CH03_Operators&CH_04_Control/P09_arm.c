a2eb515e3d3476b6fadce96e53b37c92c7a18651
Tanvi Darji <darjitanvi642@gmail.com>
2026-05-04 12:51:40 +0530

    arm

--- MERN-1808/CLang/CH03_Operators&CH_04_Control/P09_arm.c
+++ MERN-1808/CLang/CH03_Operators&CH_04_Control/P09_arm.c
@@ -1,40 +1,39 @@
 #include<stdio.h>
-#include<math.h>
 
-int main(){
+int main() {
 
-    int num, temp, digits=0, rem, arm=0;
+    int num, temp, digits = 0, rem, arm = 0;
 
-    printf("Enter the number :");
+    printf("Enter the number: ");
     scanf("%d", &num);
 
     temp = num;
 
-    // Handle 0 case
-    if (temp == 0) {
-        digits = 1;
-    } else {
-        while (temp > 0)
-        {
-            digits++;
-            temp /= 10;
-        }
-    }
+    do {
+        digits++;
+        temp /= 10;
+    } while (temp != 0);
 
     temp = num;
 
-    while (num > 0)
-    {
-        rem = num % 10;   
-        arm = arm + (int)pow(rem, digits);
-        num = num / 10;
+    do {
+        rem = num % 10;
+
+        int power = 1;
+        for(int i = 0; i < digits; i++) {
+            power *= rem;
+        }
+
+        arm += power;
+        num /= 10;
+
+    } while (num != 0);
+
+    if (temp == arm) {
+        printf("%d is an Armstrong number\n", temp);
+    } else {
+        printf("%d is not an Armstrong number\n", temp);
     }
 
-    if(temp == arm) {
-        printf("%d is an armstrong number.\n", temp);
-    } else {
-        printf("%d is not an armstrong number.\n", temp);
-    }
-    
     return 0;
 }
