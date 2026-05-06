7362c33df5034708f9b58ca27b5bf3c685948d4e
Tanvi Darji <darjitanvi642@gmail.com>
2026-05-06 11:15:35 +0530

    basic

---
+++ ai-2207/python/CH01_Basic/P01_sum.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Addition operation
+sum = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum)
+# print(f"The sum of {num1} and {num2} is: {sum}")
