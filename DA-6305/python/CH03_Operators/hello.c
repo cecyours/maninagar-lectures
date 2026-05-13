4faf31789fc51cd8435c98bc58dbfecc6bb9aa6b
Tanvi Darji <darjitanvi642@gmail.com>
2026-05-13 11:51:45 +0530

    expressions

---
+++ DA-6305/python/CH03_Operators/hello.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= '0' && ch <= '9') {
+        printf("It is a Number");
+    }
+    else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+        
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + 32;
+        }
+
+        switch(ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                printf("It is a Vowel");
+                break;
+            default:
+                printf("It is a Consonant");
+        }
+    }
+    else {
+        printf("Invalid Character");
+    }
+
+    return 0;
+}
