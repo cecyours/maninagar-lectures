6e0295357c5781ce3feb40093492d732e718a12a
Tanvi Darji <darjitanvi642@gmail.com>
2026-06-02 17:12:55 +0530

    Const

---
+++ mern-2009/c/CH02_data_type/P04_marks.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+
+    float marks;
+
+    printf("Enter marks: ");
+    scanf("%f", &marks);
+
+    printf("MARKS: %f", marks);
+}
---
+++ mern-2009/c/CH02_data_type/P05_defult.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main() {
+    int i = 10;
+
+    printf("Default value: %d\n",i);
+
+    i = 100;
+
+    printf("New value: %d\n",i);
+    
+}
