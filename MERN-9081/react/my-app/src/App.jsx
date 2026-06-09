d7a480178df3ef30e1a282dd267d87a9610cdb91
Tanvi Darji <darjitanvi642@gmail.com>
2026-06-09 09:50:37 +0530

    props

--- MERN-9081/react/my-app/src/App.jsx
+++ MERN-9081/react/my-app/src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import User from './components/User'
 
 function App() {
-  const name = "Nasrin";
-  const currentDate = new Date().toLocaleDateString();
+  // const name = "Nasrin";
+  // const currentDate = new Date().toLocaleDateString();
 
   return (
     <>
     <div>
       <h1>Welcome to react!</h1>
-      <p>Hello, {name}! Today is {currentDate}.</p>
-      <button onClick={() => alert("You clicked the button!")}>Click Me</button>
+      {/* <p>Hello, {name}! Today is {currentDate}.</p>
+      <button onClick={() => alert("You clicked the button!")}>Click Me</button> */}
+      <User name="Tanvi" age={23}/>
+      <User name="Nasrin" age={18}/>
+      <User name="bgh" age={12}/>
     </div>
     </>
   )
---
+++ MERN-9081/react/my-app/src/components/User.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function User({name, age}) {
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        margin: "10px",
+        padding: "10px",
+        borderRadius: "5px",
+      }}
+    >
+      <h3>User Name: {name}</h3>
+      <p>Age: {age}</p>
+    </div>
+  );
+}
+
+export default User;
