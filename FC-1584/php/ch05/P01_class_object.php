cc87a72dbf9bbb548c014e6ea72fa044dba99ddf
Tanvi Darji <darjitanvi642@gmail.com>
2026-06-10 12:19:47 +0530

    OOP

---
+++ FC-1584/php/ch05/P01_class_object.php
@@ -0,0 +1,19 @@
+<?php
+// CLASS & OBJECT
+class Student
+{
+    public $name; // member variable
+
+
+    public function showName()
+    { // member function
+        echo "Name: " . $this->name;
+    }
+}   
+
+
+$obj = new Student();
+$obj->name = "Tanvi";
+$obj->showName();
+
+?>
