0daabc87dc02c5b79c7570fddde690fb77f7d299
Tanvi Darji <darjitanvi642@gmail.com>
2026-06-19 12:07:57 +0530

    Function

---
+++ MERN-1808/CLang/CH07_Function/p02_call.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void display() // function definition
+{
+    printf("Hello World\n");
+}
+void main() // main function
+{
+    
+    display(); // calling display()
+    display(); // calling display()
+   
+}
