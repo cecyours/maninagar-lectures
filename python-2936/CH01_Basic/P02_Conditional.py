f4232d6c321daa583affc5b99ec7a988767dba71
Tanvi Darji <darjitanvi642@gmail.com>
2026-06-27 10:15:11 +0530

    Done

---
+++ python-2936/CH01_Basic/P02_Conditional.py
@@ -0,0 +1,37 @@
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("This number is positive...")
+elif num < 0:
+    print("This number is negative...")
+else:
+    print("This number is positive...")
+    
+    
+# Case Sensitivity
+Var = 3
+var = 2
+print(Var)  
+print(var)  
+
+# Statements and Expressions
+X = 10;
+Y = X + 5
+print(Y)
+
+# End of Statement
+a = 15; b=10; print(a+b)
+
+# indentation in if
+num = 19
+if num > 0:
+    print("Number is positive")
+    print("This is inside the if block")
+print("This is outside the if block")
+
+#  Indentation in loops
+for i in range(1, 4):
+    print("Iteration:", i)
+    print("Inside the loop")
+print("Loop ended")
+
