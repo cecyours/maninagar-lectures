7d72f99d259a973dac0a404664dfa2c20e3c5b61
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-06 16:57:56 +0530

    Arithmetic

---
+++ python-2936/CH03_Operators/P01_Arithmetic.py
@@ -0,0 +1,10 @@
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
