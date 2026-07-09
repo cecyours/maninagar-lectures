e467c59836220a469dfac68f9c237dff694f29a5
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-09 17:51:08 +0530

    int-sum

---
+++ gamed-2015/clang/CH02_Data_Type/P01_int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int a = 10;
+    int b = -5;
+    int c = a + b;
+
+    printf("Value of a: %d\n", a);
+    printf("Value of b: %d\n", b);
+    printf("Sum of %d and %d = %d", a,b,c);
+
+    return 0;
+}
