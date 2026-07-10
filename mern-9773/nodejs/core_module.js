f3a96e67885ee935eee74236d0180e155da9a797
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-10 15:46:26 +0530

    core-module

---
+++ mern-9773/nodejs/core_module.js
@@ -0,0 +1,28 @@
+import http from 'http'
+import fs from 'fs'
+
+fs.writeFileSync('mine.txt', "hello, how are you?",)
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+
+
+
+
+    const url = req.url
+
+    switch (url) {
+        case '/about':
+            res.write("welcome to about page")
+            break
+        case '/contact':
+            res.write("welcome to contact page")
+    }
+
+    res.end();
+})
+
+server.listen(4000, () => {
+    console.log("server is running on 4000");
+})
+
