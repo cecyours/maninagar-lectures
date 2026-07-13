a3d776728b42a41805244ffbbbad166e92ae2e19
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-13 11:50:42 +0530

    done

---
+++ DA-0825/CH01_Basic/P04_Indentation.py
@@ -0,0 +1,24 @@
+# Var = 5
+# var = 10
+# print(Var)
+# print(var)
+
+# Statement
+x = 10  # assignment statement
+
+# Expression
+y = x + 5
+print(y)
+
+a = 5; b = 10; print(a + b)
+
+num = 10
+
+if num > 0:
+    print("Number is positive")
+    print("This is inside the if block")
+print("This is outside the if block")
+
+for i in range(1, 11):
+    print( i)
+ 
---
+++ DA-0825/CH01_Basic/P05_Syntax.py
@@ -0,0 +1,9 @@
+# Program to check whether a number is even or odd
+
+num = int(input("Enter a number: "))  # Take input from user
+
+# Check if number is even
+if num % 2 == 0:
+    print("The number is even")  # Print if even
+else:
+    print("The number is odd")   # Print if odd
