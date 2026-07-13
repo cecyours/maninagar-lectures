5bd3b52febad141c512e50fbfab50dbc370d6890
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-13 18:15:50 +0530

    float-double

---
+++ gamed-2015/clang/CH02_Data_Type/P02_float.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+   
+    float radius = 2.4;
+
+    float area = 3.14 * radius * radius;
+
+    printf("Radius: %f\n", radius);
+    printf("area: %f\n", area);
+
+
+    return 0;
+}
---
+++ gamed-2015/clang/CH02_Data_Type/P03_double.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+   
+    
+    double num1 = 3.122222; 
+    double num2 = 78.122222; 
+    double sum = num1 + num2;
+    
+    printf("Value of num1: %lf\n",num1);
+    printf("Value of num2: %lf\n",num2);
+    printf("sum: %lf\n",sum);
+
+
+    return 0;
+}
