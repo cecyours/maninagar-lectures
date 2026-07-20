1ecc43f455ef7498eb26b76f1c9dd0feb4fa518b
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-20 18:18:49 +0530

    extern

---
+++ gamed-2015/clang/CH02_Data_Type/P07_extern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int num = 100; // global variable
+
+void display();
+
+int main() {
+
+    extern int num;
+
+    printf("num in main = %d\n", num);
+
+    display();
+
+    return 0;
+}
+void display() {
+    
+    printf("num in display = %d\n", num);
+
+}
