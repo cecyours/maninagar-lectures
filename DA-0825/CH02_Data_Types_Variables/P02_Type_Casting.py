be79cb488828a3fedbc7686721383e2fc766dd2f
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-20 11:40:59 +0530

    type-casting

---
+++ DA-0825/CH02_Data_Types_Variables/P02_Type_Casting.py
@@ -0,0 +1,20 @@
+# Convert float to integer
+
+num = 10.75
+num_int = int(num)
+
+print(num, "->", num_int)
+
+# Convert integer to float
+
+num2 = 5
+num_float = float(num2)
+
+print(num2, "->", num_float)
+
+# Convert number to string
+
+age = 20
+age_str = str(age)
+
+print(age, "->", age_str, type(age_str))
