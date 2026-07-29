b0f192356a5abb4ec88099cef0a4942fe17e4942
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-29 18:11:33 +0530

    Type Conversion

---
+++ gamed-2015/clang/CH02_Data_Type/P09_Implicit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 10;
+    float b = 12.3;
+    float result;
+
+    result = a + b;
+    printf("Result = %f\n", result);
+
+    return 0; 
+}
---
+++ gamed-2015/clang/CH02_Data_Type/P10_Explicit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float a = 12.2;
+    int b;
+
+    b = (int)a;
+    printf("Float a = %f\n", a);
+    printf("Integer B = %d\n", b);
+
+    return 0;
+
+}
