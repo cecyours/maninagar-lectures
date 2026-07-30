45d06a273e814416e79311ca6865838b2f823f04
Tanvi Darji <darjitanvi642@gmail.com>
2026-07-30 17:00:49 +0530

    Relational

---
+++ mern-2009/c/CH03_Oprators/P03_relational.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+    int a, b;
+
+    printf("Enter two number: ");
+    scanf("%d,%d", &a, &b);
+
+    printf("%d > %d : %d\n", a, b, a>b);
+    printf("%d < %d : %d\n", a, b, a<b);
+    printf("%d >= %d : %d\n", a, b, a>=b);
+    printf("%d <= %d : %d\n", a, b, a<=b);
+    printf("%d != %d : %d\n", a, b, a!=b);
+    printf("%d == %d : %d\n", a, b, a==b);
+
+    
+    
+}
