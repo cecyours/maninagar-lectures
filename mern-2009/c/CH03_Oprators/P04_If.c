80adf2a75e6e9f82f812d26f23677d8c7bcde55a
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-03 17:14:01 +0530

    if

---
+++ mern-2009/c/CH03_Oprators/P04_If.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main(){
+    int a, b;
+
+    printf("Enter Two Numbers: ");
+    scanf("%d,%d", &a, &b);
+
+    if ( a > b )
+    {
+        printf("A is max");
+    } else {
+        printf("B is max");
+    }
+    
+    
+}
