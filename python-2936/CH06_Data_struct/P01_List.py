d8720f37dd73748278a4acd781f29af50c99a0ec
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-04 18:06:26 +0530

    list

---
+++ python-2936/CH06_Data_struct/P01_List.py
@@ -0,0 +1,51 @@
+# Creating
+numbers = [10, 20, 30, 40, 50]
+print(numbers)
+
+data = [25, "Python", 3.14, True]
+print(data)
+
+items = []
+print(items)
+
+# Accessing Elements
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print(fruits[0])
+print(fruits[2])
+
+# Negative Indexing
+numbers = [10, 20, 30, 40]
+print(numbers[-1])
+print(numbers[-2])
+
+# Changing 
+colors = ["Red", "Blue", "Green"]
+colors[1] = "Yellow"
+print(colors)
+
+# append()
+numbers = [1, 2, 3]
+numbers.append(4)
+print(numbers)
+
+# insert()
+numbers = [10, 20, 30]
+numbers.insert(1, 15)
+print(numbers)
+
+# remove()
+fruits = ["Apple", "Banana", "Mango"]
+fruits.remove("Banana")
+print(fruits)
+
+# pop()
+numbers = [5, 10, 15, 20]
+numbers.pop(2)
+print(numbers)
+
+# Managing a List of Students
+students = ["Amit", "Doe", "Mary"]
+print("Original List:", students)
+students.append("Neha")
+students[1] = "Rohan"
+print("Updated List:", students)
