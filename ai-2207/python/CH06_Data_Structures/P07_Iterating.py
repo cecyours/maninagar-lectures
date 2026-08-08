73561f08a09005b35feb6b3b99a2af6cdc57ad64
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-08 12:35:13 +0530

    List

---
+++ ai-2207/python/CH06_Data_Structures/P07_Iterating.py
@@ -0,0 +1,16 @@
+# for
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    print(num)
+
+# range()
+numbers = [10, 20, 30, 40]
+for i in range(len(numbers)):
+    print("Index:", i, "Value:", numbers[i])
+
+# while
+numbers = [5, 10, 15, 20]
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
