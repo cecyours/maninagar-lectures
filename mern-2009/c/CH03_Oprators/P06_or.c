135c994adb035929d939752ce8746dfe4d948ff3
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-11 17:15:34 +0530

    or

---
+++ mern-2009/c/CH03_Oprators/P06_or.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    char ch;
+
+    printf("Enter Character: ");
+    scanf("%c", &ch);
+
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        printf("%c is alphabet. \n", ch);
+    } else {
+        printf("%c is not alphabet. \n", ch);
+
+    }
+
+   return 0;
+}
