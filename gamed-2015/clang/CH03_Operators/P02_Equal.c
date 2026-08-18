905944b6f0e6eb5f2bfc136f4c2a8514430d1450
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-18 18:02:38 +0530

    equal-not equal

---
+++ gamed-2015/clang/CH03_Operators/P02_Equal.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter Value of A: ");
+    scanf("%d", &a);
+
+    printf("Enter Value of B: ");
+    scanf("%d", &b);
+
+    printf("a == b %d\n", a == b);
+
+    return 0;
+}
+
+// 1 = true
+// 0 = false
---
+++ gamed-2015/clang/CH03_Operators/P03_Not_Equal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter Value of A: ");
+    scanf("%d", &a);
+
+    printf("Enter Value of B: ");
+    scanf("%d", &b);
+
+    printf("a != b %d\n", a != b);
+
+    return 0;
+}
+
