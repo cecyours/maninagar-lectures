06e1708aa5bd994ecad19fa0fc6a67ecbc48dbb2
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-20 10:35:02 +0530

    get

--- MERN-9081/expressjs/myserver/routes/users.js
+++ MERN-9081/expressjs/myserver/routes/users.js
@@ -1,9 +1,99 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const students = [
+  {
+    id: 1,
+    name: "Aarav Sharma",
+    age: 10,
+    grade: "10th",
+    email: "aarav.sharma@example.com",
+  },
+  {
+    id: 2,
+    name: "Mia Johnson",
+    age: 11,
+    grade: "11th",
+    email: "mia.johnson@example.com",
+  },
+  {
+    id: 3,
+    name: "Liam Williams",
+    age: 12,
+    grade: "12th",
+    email: "liam.williams@example.com",
+  },
+  {
+    id: 4,
+    name: "Sophia Brown",
+    age: 10,
+    grade: "10th",
+    email: "sophia.brown@example.com",
+  },
+  {
+    id: 5,
+    name: "Noah Jones",
+    age: 11,
+    grade: "11th",
+    email: "noah.jones@example.com",
+  },
+  {
+    id: 6,
+    name: "Isabella Garcia",
+    age: 12,
+    grade: "12th",
+    email: "isabella.garcia@example.com",
+  },
+  {
+    id: 7,
+    name: "Elijah Miller",
+    age: 10,
+    grade: "10th",
+    email: "elijah.miller@example.com",
+  },
+  {
+    id: 8,
+    name: "Neha Joshi",
+    age: 14,
+    grade: "8th",
+    email: "neha.joshi@example.com",
+  },
+  {
+    id: 9,
+    name: "Vikram Rao",
+    age: 18,
+    grade: "12th",
+    email: "vikram.rao@example.com",
+  },
+  {
+    id: 10,
+    name: "Tanya Kapoor",
+    age: 13,
+    grade: "10th",
+    email: "tanya.kapoor@example.com",
+  },
+];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.json(students);
+});
+
+// get student by id
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const student = students.find((std) => std.id === id);
+
+  if (!student) return res.status(404).json({ message: "Student not found" });
+
+  res.json(student);
+});
+
+// dynamic routes with params
+router.get("/email/:emailId", function (req, res, next) {
+  const { emailId } = req.params;
+  const filterdStudent = students.filter((stu) => stu.email === emailId);
+  res.json(filterdStudent);
 });
 
 module.exports = router;
