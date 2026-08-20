1211f8a26d46a759cdf91ae2b17a920a217e8ae6
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-20 17:34:25 +0530

    Assignment

---
+++ mern-2009/c/CH03_Oprators/P09_Assignment.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, c;
+
+    printf("Enter 2 numbers : ");
+    scanf("%d,%d", &a, &b);
+
+    a += b; // a = a+b;
+    printf("value of a after += : %d\n", a);
+
+    a *= b; // a = a*b;
+    printf("value of a after *= : %d\n", a);
+    
+    a <<= b; // a = a<<b;
+    printf("value of a after <<= : %d\n", a);
+    
+    a >>= b; // a = a&b;
+    printf("value of a after >>= : %d\n", a);
+
+    a %= 10; // a = a%10;
+    printf("value of a after %%= : %d\n", a);
+
+}
