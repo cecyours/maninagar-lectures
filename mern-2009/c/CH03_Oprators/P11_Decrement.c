cdb1cf10caebd8f2421b311d5db44f73c4cb5aa0
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-25 17:21:32 +0530

    Done

---
+++ mern-2009/c/CH03_Oprators/P11_Decrement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 10;
+
+    printf("during post-increment : %d\n", a--);
+    printf("after pre-increment : %d\n", a);
+    
+    int i = 10;
+    printf("during pre-increment : %d\n", --i);
+    printf("after pre-increment : %d\n", i);
+
+    
+
+}
