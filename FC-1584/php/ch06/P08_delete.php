c402d6b3ecc46eac2d937dee7cd5a7d388a5c5b1
Tanvi Darji <darjitanvi642@gmail.com>
2026-08-27 12:22:38 +0530

    Delete

---
+++ FC-1584/php/ch06/P08_delete.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "DELETE FROM MyGuests WHERE id=3";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
