e084eb2fde1be29fa7e6f9948a0ee83c8a907927
Tanvi Darji <darjitanvi642@gmail.com>
2026-09-17 11:10:56 +0530

    tuples

---
+++ DA-6305/python/CH06_Data_Sturucture/P06_Searching.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40]
+if 30 in numbers:
+    print("Element found in the list")
+else:
+    print("Element not found")
+
+# index()
+fruits = ["apple", "banana", "mango", "orange"]
+position = fruits.index("mango")
+print("Position of mango:", position)
