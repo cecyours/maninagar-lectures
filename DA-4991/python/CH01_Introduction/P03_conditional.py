6a44a215875585c58d1358896ea888bd4b464d52
Tanvi Darji <darjitanvi642@gmail.com>
2026-09-22 16:34:17 +0530

    Indentation

---
+++ DA-4991/python/CH01_Introduction/P03_conditional.py
@@ -0,0 +1,49 @@
+num = int(input("Enter a number: "))
+if num > 0:
+    print("The number is positive...")
+elif num < 0:
+    print("The number is negative...")
+else:
+    print("The number is zero...")
+    
+# case sensitivity
+
+Var = 5
+var = 10
+print(Var)
+print(var)
+
+# statements and expressions
+x = 10
+y = x + 10
+print(y)
+
+# end of statements
+a = 5; b=10; print(a+b)
+
+# Indentation
+
+num = 10
+if num < 0:
+    print("Number is positive")
+    print("This is inside the if block")
+print("This is outside the if block")
+
+# in loop
+
+for i in range(1, 5):
+    print("Iteration: ", i)
+    print("Inside the loop")
+print("loop end")
+
+# hello
+
+# Multi-line Comments
+"""hello
+hello
+hello
+ehhl
+hjjhygj
+hjgujhj
+hujhuj
+hkuhkj"""
